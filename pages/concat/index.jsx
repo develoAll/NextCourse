c7260a687ccfed286db11cbabaf9c20cf0b158a9
Alejandro Velarde <alejandrovelardenahui.02@gmail.com>
2022-06-26 01:05:06 -0500

    Sex 2, cap 20

--- pages/concat/index.jsx
+++ pages/concat/index.jsx
@@ -1,31 +1,19 @@
-import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../../styles/Home.module.css'
-import { Navbar } from '../components/Navbar'
+import { MainLayout } from '../../components/layouts/MainLayout'
 
 export default function ConcatPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Esytoy en el concat</title>
-        <meta name="description" content="Concat Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MainLayout>
+      <h1>Concat Page</h1>
 
-      <Navbar/>
-        
-      <main className={styles.main}>
-        <h1>Concat Page</h1>
+      <h1 className="title">
+        Ir a <Link href="/">Home</Link>
+      </h1>
 
-        <h1 className={styles.title}>
-          Ir a <Link href="/">Home</Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/concat.jsx</code>
-        </p>
-      </main>
-    </div>
+      <p className="description">
+        Get started by editing{' '}
+        <code className="code">pages/concat.jsx</code>
+      </p>
+    </MainLayout>
   )
 }
